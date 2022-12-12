d9fd575a03124d3b729e36c86f58f48bb54334d4
phyxtar <manthandas172@gmail.com>
2022-12-12 12:53:08 +0530

    pack done3

---
+++ frontend/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-snackbar-material-ui';
+//import AlertTemplate from 'react-alert-template-basic';
+
+import App from './App';
+
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
+ReactDOM.render(
+  <Provider store={store}>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  </Provider>,
+  document.getElementById('root')
+);
